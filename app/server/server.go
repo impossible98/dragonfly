437adb6fe904291833e2a0b33729cce4c16549b0
impossible98 <ago2chris@outlook.com>
2022-10-19 16:18:14 +0800

    feat: add autoload config

--- app/server/server.go
+++ app/server/server.go
@@ -1,15 +1,18 @@
 package server
 
 import (
+	// import built-in packages
+	"strconv"
 	// import third-party packages
 	"github.com/gin-gonic/gin"
 	// import local packages
 	"dragonfly/app/api"
+	"dragonfly/app/config"
 )
 
 func SetupServer() {
 	app := gin.Default()
 	app.GET("/api/version", api.ReadVersion)
 	app.GET("/api/healthcheck", api.ReadHealthcheck)
-	app.Run(":8080")
+	app.Run(":" + strconv.Itoa(config.ServerPort))
 }
